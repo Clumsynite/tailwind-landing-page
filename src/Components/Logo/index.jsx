463d1fef31038190fd4b8af27413cb8b2c19e61f
Rishabh Pathak <clusmsyknight@gmail.com>
2023-06-06 16:30:34 +0530

    added navbar

---
+++ src/Components/Logo/index.jsx
@@ -0,0 +1,6 @@
+function Logo() {
+  const label = "<LandingPage />";
+  return <div className="text-[#fff] text-2xl">{label}</div>;
+}
+
+export default Logo;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,87 +1,74 @@
-function Navbar() {
-  // const sections = [
-  //   {
-  //     id: "",
-  //     label: "",
-  //   },
-  // ];
+import Logo from "./Logo";
+
+import { useState } from "react";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navigationLinks = [
+    { label: "Home", url: "#" },
+    { label: "Introduction", url: "#" },
+    { label: "README", url: "#" },
+  ];
+
+  const dropdownLinks = [
+    { label: "ABOUT", url: "#" },
+    { label: "CONTACT", url: "#" },
+    { label: "SOURCES", url: "#" },
+  ];
+
   return (
-    <nav className="bg-transparent border-gray-200 dark:bg-gray-900">
-      <div className="flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-        </a>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+    <nav className="bg-transparent text-white">
+      <div className="max-w-7xl mx-auto px-4 py-2 flex items-center justify-between">
+        <div className="flex items-center space-x-4">
+          {navigationLinks.map((link) => (
+            <a key={link.label} href={link.url} className="hidden md:inline-block">
+              {link.label}
+            </a>
+          ))}
+        </div>
+        <div className="mx-auto">
+          <Logo />
+        </div>
+
+        <div className="hidden md:flex items-center space-x-4">
+          {dropdownLinks.map((link) => (
+            <a key={link.label} href={link.url}>
+              {link.label}
+            </a>
+          ))}
+        </div>
+
+        <div className="md:hidden relative">
+          <button
+            onClick={toggleMenu}
+            className="flex items-center px-3 py-2 border rounded text-gray-300 border-gray-300 hover:text-white hover:border-white"
           >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
+            <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 011-1h12a1 1 0 010 2H4a1 1 0 01-1-1zm0 5a1 1 0 011-1h12a1 1 0 010 2H4a1 1 0 01-1-1zm0 5a1 1 0 110-2h12a1 1 0 110 2H4z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
         </div>
       </div>
+
+      {isMenuOpen && (
+        <div className="md:hidden bg-transparent py-2 w-full text-center">
+          {[...navigationLinks, ...dropdownLinks].map((link) => (
+            <a key={link.label} href={link.url} className="block px-4 py-2 hover:bg-gray-800">
+              {link.label}
+            </a>
+          ))}
+        </div>
+      )}
     </nav>
   );
-}
-
+};
 export default Navbar;
