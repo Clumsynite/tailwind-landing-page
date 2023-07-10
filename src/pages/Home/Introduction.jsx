53277a677c7c88b096e1a90ee6750798ca585b2c
Rishabh Pathak <clusmsyknight@gmail.com>
2023-07-10 23:07:31 +0530

    added aos on Introduction

--- src/pages/Home/Introduction.jsx
+++ src/pages/Home/Introduction.jsx
@@ -1,6 +1,6 @@
 function Introduction() {
   return (
-    <div className="h-[100vh] flex flex-col justify-center items-center" id="introduction">
+    <div className="h-[100vh] flex flex-col justify-center items-center" id="introduction" data-aos="fade-up">
       <div className="border-[transparent] border-solid border-2 hover:border-[#576672] transition-all text-center p-6 rounded-md">
         <div className="text-8xl font-extrabold uppercase text-[#DBECE8] text_border">Welcome</div>
         <div className="text-6xl uppercase font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#DDEFEA] to-[#687D87]">
