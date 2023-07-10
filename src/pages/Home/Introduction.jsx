f178f238b007a235fc0a3f8c629bf97938c69ab8
Rishabh Pathak <clusmsyknight@gmail.com>
2023-07-10 15:19:57 +0530

    added style to show text stoke

--- src/pages/Home/Introduction.jsx
+++ src/pages/Home/Introduction.jsx
@@ -1,9 +1,9 @@
 function Introduction() {
   return (
     <div className="h-[100vh] flex flex-col justify-center items-center">
-      <div className="border-[#576672] border-solid border-2 text-center p-2 rounded-md">
-        <div className="text-8xl uppercase text-[#DBECE8]">Welcome</div>
-        <div className="text-5xl uppercase font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#DDEFEA] to-[#687D87]">
+      <div className="border-[transparent] border-solid border-2 hover:border-[#576672] transition-all text-center p-6 rounded-md">
+        <div className="text-8xl font-extrabold uppercase text-[#DBECE8] text_border">Welcome</div>
+        <div className="text-6xl uppercase font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#DDEFEA] to-[#687D87]">
           To Landing Page
         </div>
       </div>
