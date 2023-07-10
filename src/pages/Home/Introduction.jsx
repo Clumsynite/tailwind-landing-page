3fa7285456fb71f1f21a283ae3f4f84f95c98f53
Rishabh Pathak <clusmsyknight@gmail.com>
2023-07-10 15:08:28 +0530

    added gradient on introduction text

--- src/pages/Home/Introduction.jsx
+++ src/pages/Home/Introduction.jsx
@@ -1,9 +1,11 @@
 function Introduction() {
   return (
-    <div className="h-[90vh] flex flex-col justify-center items-center">
+    <div className="h-[100vh] flex flex-col justify-center items-center">
       <div className="border-[#576672] border-solid border-2 text-center p-2 rounded-md">
         <div className="text-8xl uppercase text-[#DBECE8]">Welcome</div>
-        <div className="text-5xl uppercase font-bold text-[#DBECE8]">To Landing Page</div>
+        <div className="text-5xl uppercase font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#DDEFEA] to-[#687D87]">
+          To Landing Page
+        </div>
       </div>
     </div>
   );
